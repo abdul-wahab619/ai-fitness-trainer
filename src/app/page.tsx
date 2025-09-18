d898a8d2f4b875b9d4394e564141809205ad9757
abdul-wahab619 <awminhas619@gmail.com>
2025-09-18 13:20:23 +0500

    ai-fitness-trainer: auth setup using clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import { SignedIn, SignedOut, SignInButton, SignOutButton } from "@clerk/nextjs";
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const HomePage = () => {
+  return (
+    <div className="bg-red-500">
+      HomePage
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
+    </div>
+  );
 };
 
-export default page;
+export default HomePage;
